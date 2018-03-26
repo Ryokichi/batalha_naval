a539cec1603f1ca58f03ff670d66950b026ea150
Heitor Ryokichi Nakamura <heitor.nakamura@hotmail.com>
2018-03-26 16:28:24 -0300

    jogo em estado funcional em beta

--- bn_client.c
+++ bn_client.c
@@ -5,51 +5,48 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-//#include <ctype.h>
+#include <ctype.h>
 
 #define SERVER_PORT 4242
 #define B_LEN 4096
 
-struct sockaddr_in localAddr, remoteAddr;	
+struct sockaddr_in localAddr, remoteAddr;    
 socklen_t sizeRemoteAddr;
 
 int local_sckt, remote_sckt, play_first;
+int iwork1, victory = 0;
 char buffer[B_LEN];
-char battleField[10][20];
+char battleField[10][24];
 char coord[2];
 
 
 void printField(){
-    char cy[2], c_aux;
+    system("clear");
+    char c_aux;
     int i, j;
+
+    printf("  |---------------------| ");    
+    printf("  |----- SEU CAMPO -----|");
+    printf("    |JOGADAS|\n");
     printf("  | A|B|C|D|E|F|G|H|I|J | ");
-    printf("  |----- SEU CAMPO -----|\n");
-    printf("  |---------------------| ");
-    printf("  |---------------------|\n");
-    for(i = 0; i < 10; i++){
-        memset(cy, 0x0,sizeof(char));
-        switch(i){
-            case 0: strcpy(cy," 1"); break;
-            case 1: strcpy(cy," 2"); break;
-            case 2: strcpy(cy," 3"); break;
-            case 3: strcpy(cy," 4"); break;
-            case 4: strcpy(cy," 5"); break;
-            case 5: strcpy(cy," 6"); break;
-            case 6: strcpy(cy," 7"); break;
-            case 7: strcpy(cy," 8"); break;
-            case 8: strcpy(cy," 9"); break;
-            case 9: strcpy(cy,"10"); break;
-        }        
-        printf("%s| ", cy);
+    printf("  |---------------------|");
+    printf("    |AD| |VC|\n");
+    for(i = 0; i < 10; i++){        
+        printf(" %d| ", i);
         ///Campo adversário
         for (j = 0; j < 10; j++){
             c_aux = battleField[i][j];
-            if ((c_aux == 'W') || (c_aux != '*'))
-            	c_aux = '~';
+            switch(c_aux){
+                case 'W': 
+                case 'A':
+                case 'B':
+                case 'C':
+                case 'S': c_aux = '~'; break;
+            }
             printf("%c ", c_aux);
         }
 
-        printf("| %s| ", cy);
+        printf("|%d  | ", i);
 
         ///Seu campo
         for(j = 10; j < 20; j++){
@@ -57,97 +54,137 @@ void printField(){
             if (c_aux == 'W') c_aux = '~';
             printf("%c ", c_aux);
         }        
+        printf("|    |");
+
+        for(j = 20; j < 22; j++){
+            printf("%c", battleField[i][j]);
+        }
+        printf("| |");
+        for(j = 22; j < 24; j++){
+            printf("%c", battleField[i][j]);
+        }
         printf("|\n");
     }    
-    printf("  |---------------------| ");
-    printf("  |---------------------|\n");
     printf("  | A|B|C|D|E|F|G|H|I|J | ");
+    printf("  |---------------------|\n");
+    printf("  |---------------------| ");    
     printf("  |----- SEU CAMPO -----|\n\n");
 }
 
-//void validateCoord(){
-//	int valid = 0;
-//	while(!valid){
-//
-//		if(!valid){
-//			memset(coord, 0x0, sizeof(coord));
-//			printf("Coordenadas invalidas\n");
-//			applyCoord();
-//		}
-//	}
-//}
+int upperCase(char str[]){
+    for (int i = 0; str[i] != 0; i++){
+        str[i] = toupper(str[i]);
+    }
+}
 
-//void applyCoord(){	
-//	printf("Informe suas coodenadas: ");
-//	scanf("%c", coord);
-//	validateCoord();
-//}
-
-
-void applyValidateCoord(){	
-	int valid = 0;
-	while(!valid){
-		memset(coord, 0x0, sizeof(coord));
-		printf("Informe suas coodenadas: ");
-	    scanf("%s", coord);
-
-		if(!valid){			
-			printf("Coordenadas invalidas\n");			
-		}
-	}
+void applyValidateCoord(){    
+    int valid = 0;
+    while(!valid){
+        memset(coord, 0x0, sizeof(coord));
+        printf("Informe coodenadas YX: ");
+        scanf("%s", coord);
+        upperCase(coord);        
+        printf("Y = %c | X = %c\n", coord[0], coord[1]);
+        
+        switch(coord[0]){
+            case '0': valid = 1; break;
+            case '1': valid = 1; break;
+            case '2': valid = 1; break;
+            case '3': valid = 1; break;
+            case '4': valid = 1; break;
+            case '5': valid = 1; break;
+            case '6': valid = 1; break;
+            case '7': valid = 1; break;
+            case '8': valid = 1; break;
+            case '9': valid = 1; break;
+            default:  valid = 0;
+        }
+        if (valid){            
+            switch(coord[1]){
+                case 'A': valid = 1; break;
+                case 'B': valid = 1; break;
+                case 'C': valid = 1; break;
+                case 'D': valid = 1; break;
+                case 'E': valid = 1; break;
+                case 'F': valid = 1; break;
+                case 'G': valid = 1; break;
+                case 'H': valid = 1; break;
+                case 'I': valid = 1; break;
+                case 'J': valid = 1; break;
+                default:  valid = 0;
+            }
+        }
+        if(!valid)
+            printf("Coordenadas invalidas\n");        
+    }
 }
 
 int main(){
-	system("clear");
-	
-	local_sckt = socket(AF_INET, SOCK_STREAM, 0);
-	if (local_sckt < 0)
-		printf("Falha. Nao possivel criar socket\n");
-	else
-		printf("Successo. Socket criado\n");
+    system("clear");
+    
+    local_sckt = socket(AF_INET, SOCK_STREAM, 0);
+    if (local_sckt < 0)
+        printf("Falha. Nao possivel criar socket\n");
+    else
+        printf("Successo. Socket criado\n");
 
-	remoteAddr.sin_family      = AF_INET;
-	remoteAddr.sin_port        = htons(SERVER_PORT);
-	remoteAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	memset(remoteAddr.sin_zero, 0x0, 8);
+    remoteAddr.sin_family      = AF_INET;
+    remoteAddr.sin_port        = htons(SERVER_PORT);
+    remoteAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(remoteAddr.sin_zero, 0x0, 8);
 
-	//remote_sckt = connect(local_sckt, (struct sockaddr*)&remoteAddr, sizeRemoteAddr);
-	remote_sckt = connect(local_sckt, (struct sockaddr*)&remoteAddr, sizeof(remoteAddr));
-	if (remote_sckt < 0)
-		printf("Falha ao conectar com servidor\n");
-	else{
-		printf("Successo. Conexao estabelecida com o servidor\n");
+    //remote_sckt = connect(local_sckt, (struct sockaddr*)&remoteAddr, sizeRemoteAddr);
+    remote_sckt = connect(local_sckt, (struct sockaddr*)&remoteAddr, sizeof(remoteAddr));
+    if (remote_sckt < 0)
+        printf("Falha ao conectar com servidor\n");
+    else{
+        printf("Successo. Conexao estabelecida com o servidor\n");
 
-		recv(local_sckt, buffer, sizeof(buffer), 0);
-		printf("Msg do servidor: %s\n", buffer);
+        recv(local_sckt, buffer, sizeof(buffer), 0);
+        printf("Msg do servidor: %s\n", buffer);
 
         recv(local_sckt, battleField, sizeof(battleField),0);
-		recv(local_sckt, buffer, sizeof(buffer), 0);
-		play_first = atoi(buffer);
-		printf("Valor sorteado = %d\n", play_first);
+        recv(local_sckt, buffer, sizeof(buffer), 0);
+        printField();
+        play_first = atoi(buffer);
+        printf("Valor sorteado = %d\n", play_first);
 
-		if (play_first == 0)        	
+        if (play_first == 0)            
             printf("Voce sera o segundo a jogar\n");
         else{
-        	printf("Voce joga primeiro\n");
-        	printField();
-        	applyValidateCoord();
-        }
-
+            printf("Voce joga primeiro\n");
+            printField();
+            applyValidateCoord();
+            send(local_sckt, coord, sizeof(coord), 0);
+            recv(local_sckt, battleField, sizeof(battleField), 0);
+            printField();
+        }      
         
-        
-        
-        	
-/*
-	    while(1){
-		    memset(battleField, 0x0, sizeof(battleField));
-	        recv (local_sckt, battleField, sizeof(battleField), 0);
-            printf("Msg do servidor: %s\n", battleField[0] );
+        while(!victory){
+            printf("Aguardando oponente... \n");           
+            recv(local_sckt, battleField, sizeof(battleField), 0);
 
             memset(buffer, 0x0, B_LEN);
-            scanf("%s", buffer);
-            send(local_sckt, buffer, strlen(buffer), 0);
-	    }*/
-	}	
-	printf("Conexao encerrada com servidor\n");
+            recv(local_sckt, buffer, sizeof(buffer), 0);
+            victory = atoi(buffer);
+            printField();
+
+            if (!victory){
+                applyValidateCoord();
+                send(local_sckt, coord, sizeof(coord),0);
+                recv(local_sckt, battleField, sizeof(battleField), 0);
+
+                memset(buffer, 0x0, B_LEN);
+                recv(local_sckt, buffer, sizeof(buffer), 0);
+                victory = atoi(buffer);
+                printField();
+            }
+        }
+    }
+    if(victory == 1)
+        printf("Seu adversario venceu!!!\n");
+    else
+        printf("Voce venceu!!! :)\n");
+
+    printf("Conexao encerrada com servidor\n");
 }
--- bn_server.c
+++ bn_server.c
@@ -6,6 +6,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <ctype.h>
 
 #define SERVER_PORT 4242
 #define B_LEN 4096  ////tamanho do buffer para trocar mensagens
@@ -17,28 +18,27 @@ socklen_t sizeRemoteAddr;
 ///Variáveis Globais
 int local_sckt, remote_sckt, bind_sckt;
 int i_recv, i_send, play_first = 0;
-int i, j, align, collision;
+int i, j, align, collision, victory = 0;
 char coord[2];
 char buffer[B_LEN];
 
 ////O servidor controlará as jogadas e cuidará de replicar para o cliente o status atual
-char battleField[10][20] = {
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','X',  'W','W','W','W','W','W','W','W','W','*'},
-    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W'},
-    {'W','W','W','W','W','W','W','W','W','*',  'W','W','W','W','W','W','W','W','X','W'}
+char battleField[10][24] = {
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'},
+    {'W','W','W','W','W','W','W','W','W','W',  'W','W','W','W','W','W','W','W','W','W', '-','-', '-','-'}
 };   
 
 ///-------------------------------------------------------------------------///
 ///-------------------------------------------------------------------------///
-int checkCoordinate(int i, int j){
-	printf("Checando %dx%d = %c\n",i, j, battleField[i][j] );
+int checkCoordinate(int i, int j){    
     return ((battleField[i][j]) != 'W');    
 }
 
@@ -52,7 +52,7 @@ int checkCollision(int i, int j, int align, int shipSize){
     
     switch(align){
         case 0: ///Vertical
-            for (int c = 0; c < shipSize; c++){                	
+            for (int c = 0; c < shipSize; c++){                    
                 collision = checkCoordinate((i + c) , j );
                 if (collision) goto shipCollided;
                 
@@ -75,7 +75,7 @@ int checkCollision(int i, int j, int align, int shipSize){
             }            
             break;
         case 1: ///Horizontal
-            for (int c = 0; c < shipSize; c++){                	
+            for (int c = 0; c < shipSize; c++){                    
                 collision = checkCoordinate(i, (j + c));
                 if (collision) goto shipCollided;
                     
@@ -112,8 +112,7 @@ void creatShip(int shipSize, char shipType){
         for (int c = 0; c < shipSize; c++){
             battleField[i][j+c] = shipType;
         }
-    }
-    printField();
+    }    
 }
 
 
@@ -133,24 +132,23 @@ void shuffleField(){
     align = (rand()%2);
     i = ((rand()%7));
     j = ((rand()%7)+10);
-    creatShip(4, 'A'); ///Para campo inimigo
-    printField();
+    creatShip(4, 'A'); ///Para campo inimigo    
     
 
     ///Para Battleship
     for (int s = 1; s <= 2; s++){
         collision = 1;
         align = (rand()%2);
-        while(collision){            
+        while(collision){
             i = (rand()%8);
-            j = (rand()%8);            
-            collision = checkCollision(i, j, align, 3);            
+            j = (rand()%8);
+            collision = checkCollision(i, j, align, 3);
         }
-        creatShip(3, 'B');        
+        creatShip(3, 'B');
 
         collision = 1;
         align = (rand()%2);
-        while(collision){            
+        while(collision){
             i = (rand()%8);
             j = (rand()%8+10);
             collision = checkCollision(i, j, align, 3);
@@ -160,21 +158,21 @@ void shuffleField(){
 
     ///Para Crusador
     for(int s = 0; s < 3; s++){
-    	collision = 1;
-    	align = (rand()%2);
-    	while(collision){           
+        collision = 1;
+        align = (rand()%2);
+        while(collision){
             i = (rand()%9);
-            j = (rand()%9);            
-            collision = checkCollision(i, j, align, 2);            
+            j = (rand()%9);
+            collision = checkCollision(i, j, align, 2);
         }
         creatShip(2, 'C');
 
         collision = 1;
         align = (rand()%2);
-        while(collision){            
+        while(collision){
             i = (rand()%9);
-            j = (rand()%9+10);            
-            collision = checkCollision(i, j, align, 2);            
+            j = (rand()%9+10);
+            collision = checkCollision(i, j, align, 2);
         }
         creatShip(2, 'C');
     }
@@ -182,34 +180,30 @@ void shuffleField(){
 
     //Para Submarinos
     for(int s = 0; s < 4; s++){
-    	collision = 1;
-    	align = (rand()%2);
-        while(collision){            
+        collision = 1;
+        align = (rand()%2);
+        while(collision){
             i = (rand()%10);
-            j = (rand()%10);            
-            collision = checkCollision(i, j, align, 1);            
+            j = (rand()%10);
+            collision = checkCollision(i, j, align, 1);
         }
         creatShip(1, 'S');
 
         collision = 1;
-        while(collision){            
+        while(collision){
             i = (rand()%10);
-            j = (rand()%10+10);            
+            j = (rand()%10+10);
             collision = checkCollision(i, j, align, 1);
         }
         creatShip(1, 'S');
     }
-
-    printField();
 }
-
-
 
 int tossCoin(){
     int coin;    
     coin = (((rand()%7)%3)%2);
     printf("Valor sorteado = %d\n", coin);
-    return 1;
+    return coin;
 }
 
 void msgToClient(char buffer[B_LEN]){
@@ -217,49 +211,173 @@ void msgToClient(char buffer[B_LEN]){
 }
 
 void printField(){
-    char cy[2], c_aux;
+    system("clear");
+    char c_aux;
     int i, j;
+
+    printf("  |---------------------| ");    
+    printf("  |----- SEU CAMPO -----|");
+    printf("    |JOGADAS|\n");
     printf("  | A|B|C|D|E|F|G|H|I|J | ");
-    printf("  |----- SEU CAMPO -----|\n");
-    printf("  |---------------------| ");
-    printf("  |---------------------|\n");
-    for(i = 0; i < 10; i++){
-        memset(cy, 0x0,sizeof(char));
-        switch(i){
-            case 0: strcpy(cy," 1"); break;
-            case 1: strcpy(cy," 2"); break;
-            case 2: strcpy(cy," 3"); break;
-            case 3: strcpy(cy," 4"); break;
-            case 4: strcpy(cy," 5"); break;
-            case 5: strcpy(cy," 6"); break;
-            case 6: strcpy(cy," 7"); break;
-            case 7: strcpy(cy," 8"); break;
-            case 8: strcpy(cy," 9"); break;
-            case 9: strcpy(cy,"10"); break;
-        }        
-        printf("%s| ", cy);
-        ///Campo adversário        
+    printf("  | A|B|C|D|E|F|G|H|I|J |");
+    printf("    |AD| |VC|\n");
+    for(i = 0; i < 10; i++){        
+        printf(" %d| ", i);
+        ///Campo adversário
         for (j = 10; j < 20; j++){
             c_aux = battleField[i][j];
-            if ((c_aux == 'W') || (c_aux != '*'))
-            	c_aux = '~';
+            switch(c_aux){
+                case 'W': 
+                case 'A':
+                case 'B':
+                case 'C':
+                case 'S': c_aux = '~'; break;
+            }
             printf("%c ", c_aux);
         }
 
-        printf("| %s| ", cy);
+        printf("|%d  | ", i);
 
         ///Seu campo
         for(j = 0; j < 10; j++){
             c_aux = battleField[i][j];
             if (c_aux == 'W') c_aux = '~';
             printf("%c ", c_aux);
-        }        
+        }
+        printf("|    |");
+
+        for(j = 22; j < 24; j++){
+            printf("%c", battleField[i][j]);
+        }
+        printf("| |");
+        for(j = 20; j < 22; j++){
+            printf("%c", battleField[i][j]);
+        }
         printf("|\n");
     }    
-    printf("  |---------------------| ");
-    printf("  |---------------------|\n");
     printf("  | A|B|C|D|E|F|G|H|I|J | ");
+    printf("  | A|B|C|D|E|F|G|H|I|J |\n");
+    printf("  |---------------------| ");    
     printf("  |----- SEU CAMPO -----|\n\n");
+}
+
+int upperCase(char str[]){
+    for (int i = 0; str[i] != 0; i++){
+        str[i] = toupper(str[i]);
+    }
+}
+void changeField(){
+
+}
+
+void applyValidateCoord(){    
+    int valid = 0;
+    while(!valid){
+        memset(coord, 0x0, sizeof(coord));
+        printf("Informe coodenadas YX: ");
+        scanf("%s", coord);
+        upperCase(coord);        
+        printf("Y = %c | X = %c\n", coord[0], coord[1]);
+        
+        switch(coord[0]){
+            case '0': valid = 1; break;
+            case '1': valid = 1; break;
+            case '2': valid = 1; break;
+            case '3': valid = 1; break;
+            case '4': valid = 1; break;
+            case '5': valid = 1; break;
+            case '6': valid = 1; break;
+            case '7': valid = 1; break;
+            case '8': valid = 1; break;
+            case '9': valid = 1; break;
+            default:  valid = 0;
+        }
+        if (valid){            
+            switch(coord[1]){
+                case 'A': valid = 1; break;
+                case 'B': valid = 1; break;
+                case 'C': valid = 1; break;
+                case 'D': valid = 1; break;
+                case 'E': valid = 1; break;
+                case 'F': valid = 1; break;
+                case 'G': valid = 1; break;
+                case 'H': valid = 1; break;
+                case 'I': valid = 1; break;
+                case 'J': valid = 1; break;
+                default:  valid = 0;
+            }
+        }
+        if(!valid)
+            printf("Coordenadas invalidas\n");
+    }
+    changeField();
+}
+
+void validateShot(int player){
+    int cy, cx;
+    char c_aux;
+    cy = (coord[0] - 48);
+    c_aux = coord[1];
+
+    switch(c_aux){
+        case 'A': cx = 0; break;
+        case 'B': cx = 1; break;
+        case 'C': cx = 2; break;
+        case 'D': cx = 3; break;
+        case 'E': cx = 4; break;
+        case 'F': cx = 5; break;
+        case 'G': cx = 6; break;
+        case 'H': cx = 7; break;
+        case 'I': cx = 8; break;
+        case 'J': cx = 9; break;        
+    }   
+
+    if (player == 1){
+        for (int i = 0; i < 9; i++){
+            for(int j = 20; j < 22; j++){
+                battleField[i][j] = battleField[i+1][j];
+            }
+        }
+        battleField[9][20] = coord[0];
+        battleField[9][21] = coord[1];
+    }else{
+        for (int i = 0; i < 9; i++){
+            for(int j = 22; j < 24; j++){
+                battleField[i][j] = battleField[i+1][j];
+            }
+        }
+        battleField[9][22] = coord[0];
+        battleField[9][23] = coord[1];
+    }
+
+    if (player == 1) cx += 10;    ////se for player 1 aplica coordenadas no campo 2
+    c_aux = battleField[cy][cx];
+    if (c_aux == 'A' || c_aux == 'B' || c_aux == 'C' || c_aux == 'S' || c_aux == 'X') 
+        battleField[cy][cx] = 'X';
+    else
+        battleField[cy][cx] = '*';
+}
+
+int checkVictory(int player){
+    printf("Player = %d \n", player);
+    int cont = 0;
+    int i, j, j_aux;
+    
+    for (i = 0; i < 10; i++){        
+        j = 0;
+        if (player == 1)  ///Olha campo do player 2 par ver acertos
+            j = 10;
+        j_aux = j + 10;
+
+        for ( ;j < j_aux; j++){            
+            if (battleField[i][j] == 'X') cont ++;
+        }
+    }
+    printf("Cont = %d \n", cont );
+    if (cont == 20)
+        return player;
+    else 
+        return 0;
 }
 
 int main(){
@@ -290,6 +408,7 @@ int main(){
     if (remote_sckt < 0)
         printf("Falha ao conectar com cliente\n");
     else{
+        printField();
         printf("Conexao estabelecida com cliente\n");
 
         memset(buffer, 0x0, B_LEN);
@@ -303,34 +422,49 @@ int main(){
         sprintf(buffer, "%d", play_first);
         send(remote_sckt, buffer, sizeof(buffer), 0);
 
-        printField();
 
         if (play_first == 1){
             printf("Voce sera o segundo a jogar\n\n");
             printf("Aguardando oponente...\n");
             recv(remote_sckt, coord, sizeof(coord), 0); ///É assumido que as coordenadas são validas
-
+            validateShot(2);  ///player 2
+            printf("Coordenadas informadas: %s\n", coord);
+            send(remote_sckt, battleField, sizeof(battleField), 0);
 
         }else{
             printf("Voce joga primeiro\n\n");
         }
-            
 
-
-        /*
-        while(1){            
-            memset(buffer, 0x0, B_LEN);
-            i_recv = recv(remote_sckt, buffer, B_LEN, 0);
-            if (i_recv <= 0)
-                printf("Falha ao receber msg\n");
-            printf("Msg do cliente = %s \n", buffer);
-
-            memset(buffer, 0x0, B_LEN);
-            strcpy(buffer, "Recebi sua msg O_- ");
+        while(!victory){
+            applyValidateCoord();
+            validateShot(1);  ///player 1
+            victory = checkVictory(1);
             send(remote_sckt, battleField, sizeof(battleField), 0);
-        }*/
-        memset(buffer, 0x0, B_LEN);
-        i_recv = recv(remote_sckt, buffer, B_LEN, 0);
-    }    
+
+            memset(buffer, 0x0,B_LEN);
+            sprintf(buffer, "%d", victory);
+            send(remote_sckt, buffer, sizeof(buffer), 0);
+            printField();
+
+            printf("Aguardando oponente...\n");
+
+            if(!victory){
+                recv(remote_sckt, coord, sizeof(coord),0);
+                validateShot(2);  ///player 2
+                victory = checkVictory(2);
+                send(remote_sckt, battleField, sizeof(battleField), 0);
+
+                memset(buffer, 0x0,B_LEN);
+                sprintf(buffer, "%d", victory);
+                send(remote_sckt, buffer, sizeof(buffer), 0);
+                printField();
+            }
+        }        
+    }
+    if(victory == 1)
+        printf("Voce venceu!!! :)\n");
+    else
+        printf("Seu adversario venceu!!! \n");
+
     printf("Conexao encerrada com cliente\n");
 }
